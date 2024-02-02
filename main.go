8c243320bcd8cc07b080427f5a2cea71f3d8d5df
s809616134 <s809616134@gmail.com>
2024-02-02 11:59:25 +0800

    Implement CRUD

--- main.go
+++ main.go
@@ -3,52 +3,44 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/s809616134/hotel-backend/api"
-	"github.com/s809616134/hotel-backend/types"
+	"github.com/s809616134/hotel-backend/db"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const dburi = "mongodb://localhost:27017"
-const dbname = "hotel-reservation"
-const userColl = "users"
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
 
 func main() {
+	listenAddr := flag.String("listenAddr", ":5000", "The listen addr of the api server")
+	flag.Parse()
+
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	coll := client.Database(dbname).Collection((userColl))
 
-	user := types.User{
-		FirstName: "james",
-		LastName:  "dude",
-	}
-	res, err := coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
-	fmt.Println(res)
+	app := fiber.New(config)
+	apiv1 := app.Group("/api/v1")
 
-	listenAddr := flag.String("listenAddr", ":5000", "The listen addr of the api server")
-	flag.Parse()
+	apiv1.Post("/user", userHandler.HandlePostUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
 
-	app := fiber.New()
-
-	apiv1 := app.Group("/api")
-
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/:id", api.HandleGetUser)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
+	apiv1.Delete("/user/:id", userHandler.HandleDeleteUser)
+	apiv1.Put("/user/:id", userHandler.HandlePutUser)
 
 	app.Listen(*listenAddr)
-}
-
-func handleUser(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"user": "John"})
 }
